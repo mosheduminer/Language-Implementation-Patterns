4d1b6412ae93f0eaf59a63c9f2230c0b9ffacee2
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2019-10-07 19:17:39 -0400

    Added comments
    Also added default constructor for `Token`

--- RecursiveDescentParser/lexer.h
+++ RecursiveDescentParser/lexer.h
@@ -1,4 +1,14 @@
-#include <iostream>
+/*
+	This header file declares the `Token` and `RecursiveDescentLexer` classes.
+	The `RecursiveDescentLexer` class parses a given string, by use of the 
+	`nextToken` method. The `Token` class is used to bundle token information
+	into a single object.
+
+
+	Implementation is probably inefficient, specifically that of the `Token` class. 
+*/
+
+#include <iostream> // for using std::string
 
 #pragma once
 
@@ -7,27 +17,32 @@ namespace lexer {
 	class Token
 	{
 	public:
-		int type;
-		std::string text;
-		Token(int type, std::string text);
-		std::string toString();
+		// the default class constructor is here so a variable of type `Token` can be initialized without values
+		Token();
+		Token(int type, std::string text); // the class constructor.
+		int type; // type is an int, appropiate constants are defined in the definition file.
+		std::string text; // this is the input-text that evaluated to this token.
+		std::string toString(); // evaluates the token text and type to a string.
 	};
 
 	class RecursiveDescentLexer
 	{
 	public:
-		RecursiveDescentLexer(std::string input);
-		void match(char x);
-		Token nextToken();
+		RecursiveDescentLexer(std::string input); // the class constructor.
+		void match(char x); // asserts that (c == x), and increments the cursor.
+		
+		/* `nextToken` returns the next token in the stream.
+		should be called repeatedly to tokenize the entire input */
+		Token nextToken(); 
 
 	private:
-		std::string input;
-		unsigned p = 0;
-		char c;
-		void consume();
-		void WS();
-		bool isLETTER();
+		std::string input; // the input string
+		unsigned p = 0;    // this is the cursor
+		char c;            // this is the character at cursor position
+		void consume();    // moves the cursor one space
+		void WS();         // moves the cursor past any whitespace
+		bool isLETTER();   // checks if `c` is a letter
 
-		Token NAME();
+		Token NAME();      // returns a token of type tNAME
 	};
 }
