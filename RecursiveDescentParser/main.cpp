753c3f914221e45b1bb30b755ded5990243e68f3
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2019-10-07 19:19:32 -0400

    Added comments
    Also streamlined code execution.

--- RecursiveDescentParser/main.cpp
+++ RecursiveDescentParser/main.cpp
@@ -1,33 +1,57 @@
-// Lexer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// use it to tokenize and output a specified file
+// the program must be invoked with a filename provided
 
-#include <iostream>
+#include <iostream> // for using string, cout, cerr, endl
+#include <fstream> // for file reading (ifstream) and related errors (strerror_s)
 #include "Lexer.h"
 
-int main()
+using namespace std; // so I don't have to type std:: all over the place
+
+
+extern const int tEOF; // this constant is defined in lexer.cpp, it is used here to check for EOF
+
+int main(int argc, char* argv[])
 {
-	extern const int tEOF;
-
-	std::string testString = "hello, how are you";
-	
-	lexer::RecursiveDescentLexer L = lexer::RecursiveDescentLexer(testString);
-	lexer::Token t = L.nextToken();
-	while (true) {
-		std::cout << t.toString() << std::endl;
-		if (t.type == tEOF) {
-			break;
-		}
-		t = L.nextToken();
+	// check that a filename was specified as a command line arguement, else terminate execution
+	if (argc == 1) {
+		cerr << "No file name supplied" << endl;
+		exit(0);   // call system to stop
 	}
+
+	// attempt to open the specified file
+	ifstream inFile;
+	inFile.open(argv[1]);
+
+	// check that the file was opened, else return error and terminate execution
+	if (!inFile) {
+		char e[100];
+		strerror_s(e, errno);
+		cerr << "Unable to open file " << argv[1] << ": "<< e << endl;
+		exit(0);   // call system to stop
+	}
+
+	inFile >> std::noskipws; // this ensures that whitespace is not skipped.
+
+	// declare `c` for file input streaming. used to construct `inputString`
+	char c;
+	// declare `inputString`. used in initializing the `RecursiveDescentLexer`
+	string inputString;
+
+	// stream file and build `inputString`
+	inFile >> c;
+	do {
+		inputString.push_back(c);
+	} while (inFile >> c);
+	inFile.close(); // close file
+	
+	// initialize lexer
+	lexer::RecursiveDescentLexer L = lexer::RecursiveDescentLexer(inputString);
+	// initialize token variable
+	lexer::Token t;
+	// parse and output a string representation of all tokens
+	do {
+		t = L.nextToken();
+		cout << t.toString() << endl;
+	} while (t.type != tEOF);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
